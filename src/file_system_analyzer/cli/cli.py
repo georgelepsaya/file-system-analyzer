f05a53453691ce3c955cd49ba47fb3cc49ac872f
Georgy Lepsaya <georgelepsaya@gmail.com>
2025-05-25 21:11:27 +0300

    docs: docstrings for cli and cli utils

--- src/file_system_analyzer/cli/cli.py
+++ src/file_system_analyzer/cli/cli.py
@@ -8,7 +8,12 @@ from .utils import parse_output, convert_to_bytes
 from ..logging_config import logger
 
 
-def main():
+def main() -> None:
+    """
+    Main function which is an entry for the `fsa` command. Contains CLI interaction functionality.
+    :return: None
+    """
+    # collect and parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", help="directory to be analyzed", required=True)
     parser.add_argument("-t", "--threshold",
@@ -16,20 +21,24 @@ def main():
                         type=str, required=True)
     args = parser.parse_args()
 
+    # check whether provided path exists, is a directory and is accessible
     if not (os.path.exists(args.directory)
             and os.path.isdir(args.directory)
             and os.access(args.directory, os.R_OK | os.X_OK)):
         logger.error(f"Invalid directory path provided: {args.directory}")
         sys.exit(1)
 
+    # attempt to convert provided threshold to bytes
     try:
         threshold = convert_to_bytes(args.threshold)
     except ValueError as e:
         logger.error(f"Error when parsing threshold value: {e}")
         sys.exit(1)
 
+    # initialise the file system analyzer
     fsa = FileSystemAnalyzer(args.directory, threshold)
 
+    # categorize files and show a spinner while the process is running
     console = Console()
     with console.status("[bold]Categorizing files...[/bold]", spinner="dots"):
         try:
--- src/file_system_analyzer/cli/utils.py
+++ src/file_system_analyzer/cli/utils.py
@@ -6,6 +6,7 @@ from typing import Dict
 from ..logging_config import logger
 
 
+# used when converting threshold size to bytes
 SIZE_MULTIPLIERS = {
     "B": 1,
     "KiB": 1024,
@@ -16,16 +17,23 @@ SIZE_MULTIPLIERS = {
 }
 
 
-def validate_permissions(permissions: Dict):
+def validate_permissions(permissions: Dict) -> bool:
+    """
+    Ensures the format of the provided `permissions` dictionary is correct.
+    :param permissions: Dict
+    :return: bool
+    """
     expected_structure = {
         'usr': {'r': bool, 'w': bool, 'x': bool},
         'grp': {'r': bool, 'w': bool, 'x': bool},
         'oth': {'r': bool, 'w': bool, 'x': bool}
     }
 
+    # check if provided permissions have 'usr', 'grp' and 'oth' keys
     if set(permissions.keys()) != set(expected_structure.keys()):
         return False
 
+    # for every category check that it has 'r', 'w' and 'x' keys
     for category in expected_structure:
         for permission in expected_structure[category]:
             if permission not in permissions[category]:
@@ -37,6 +45,11 @@ def validate_permissions(permissions: Dict):
 
 
 def create_table() -> Table:
+    """
+    Creates a rich.table Table with the predefined columns 'Size', 'File path' and 'Permissions'
+    :return: Table
+        rich Table object
+    """
     table = Table()
     table.add_column("Size", justify="left", no_wrap=True, header_style="bold blue")
     table.add_column("File path", justify="left", header_style="bold blue")
@@ -45,17 +58,29 @@ def create_table() -> Table:
 
 
 def parse_permissions(permissions: Dict, is_unusual: bool) -> str:
+    """
+    Turns the permissions dictionary into a formatted string which can be nicely added to the standard output
+    :param permissions: Dict
+        represents permissions for every category 'usr', 'grp' and 'oth'
+    :param is_unusual: bool
+        whether the permissions are unusual or not
+    :return: str
+    """
     try:
-        if not isinstance(permissions, dict):
+        # validate provided permissions
+        if not isinstance(permissions, Dict):
             raise ValueError("permissions must be a dictionary")
         if not validate_permissions(permissions):
             raise ValueError("permissions dictionary structure is incorrect")
+
         permissions_text = ""
+
+        # add 'r', 'w', 'x' permissions for every category
         for cat, rights in permissions.items():
-            if not isinstance(rights, dict):
+            if not isinstance(rights, Dict):
                 raise ValueError(f"rights for category {cat} must be a dictionary")
             permissions_text += f"{cat}:{''.join([k for k,v in rights.items() if v])} "
-        permissions_text.format(permissions_text)
+
         if is_unusual:
             return f"[red]{permissions_text}(unusual permissions)[/red]"
         return permissions_text
@@ -67,7 +92,19 @@ def parse_permissions(permissions: Dict, is_unusual: bool) -> str:
         raise
 
 
-def parse_output(console, output: Dict, large_files: Dict, unusual_permissions_files: Dict):
+def parse_output(console, output: Dict, large_files: Dict, unusual_permissions_files: Dict) -> None:
+    """
+    Parse output (files grouped by category), large files and files with unusual permissions
+    :param console: rich.console Console object
+        Console to which parsed output is written
+    :param output: Dict[str, CategoryFiles]
+        Output dictionary containing files grouped by category
+    :param large_files: Dict[os.PathLike, str]
+        Dictionary of large files with their paths and sizes
+    :param unusual_permissions_files: Dict[os.PathLike, List[str]]
+        Dictionary of files with unusual permissions with their paths and permission names
+    :return: None
+    """
     try:
         if not isinstance(output, dict):
             raise ValueError("output must be a dictionary")
@@ -79,10 +116,12 @@ def parse_output(console, output: Dict, large_files: Dict, unusual_permissions_f
             if not files.files:
                 continue
 
+            # craft a title for the current category along with its size and display a rich Panel
             category_text = f"{file_type.capitalize()} - {files.converted_size}"
             console.print(Panel(category_text, expand=True), style="medium_turquoise")
             table = create_table()
 
+            # add rows to the current category table
             for file in files.files:
                 if not hasattr(file, 'converted_size') or not hasattr(file, 'path') or not hasattr(file, 'processed_permissions'):
                     raise ValueError("file must have 'converted_size', 'path' and 'processed_permissions' attributes")
@@ -94,11 +133,13 @@ def parse_output(console, output: Dict, large_files: Dict, unusual_permissions_f
 
             console.print(table)
 
+        # parse all large files
         if large_files:
             console.print(Panel("Large files", expand=True), style="light_salmon3")
             for i, (k, v) in enumerate(large_files.items(), start=1):
                 console.print(f"{i}. {k}: [light_salmon3]{v}[/light_salmon3]", highlight=False)
 
+        # parse all files with unusual permissions
         if unusual_permissions_files:
             console.print(Panel("Files with unusual permissions", expand=True), style="red")
             for i, (k, v) in enumerate(unusual_permissions_files.items(), start=1):
@@ -112,7 +153,15 @@ def parse_output(console, output: Dict, large_files: Dict, unusual_permissions_f
 
 
 def convert_to_bytes(size_str: str) -> int:
+    """
+    Convert string size with units to size integer size in bytes
+    :param size_str: str
+        Size with a unit (B, KiB, MiB, GiB, TiB, PiB)
+    :return: int
+        Size in bytes
+    """
     try:
+        # validate provided size with units
         size_match = re.match(r'^(\d+)(B|KiB|MiB|GiB|TiB|PiB)?$', size_str)
         if not size_match:
             raise ValueError(f"Invalid size format: {size_str}")
@@ -123,6 +172,7 @@ def convert_to_bytes(size_str: str) -> int:
         if unit not in SIZE_MULTIPLIERS:
             raise ValueError(f"Invalid unit: {unit}")
 
+        # multiply number part of the provided size by its multiplier
         return int(number) * SIZE_MULTIPLIERS[unit]
     except ValueError as ve:
         logger.error(f"Value error converting size to bytes: {ve}")
